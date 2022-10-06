4f621f7b1c9135ec8dbb701a022312bdb00d82d2
李俊琦 <77267563@qq.com>
2022-10-06 17:25:00 +0800

    .

--- src/views/my/tmp/index.vue
+++ src/views/my/tmp/index.vue
@@ -10,36 +10,36 @@
 // ◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎◀︎▶︎
 // --------------------------
 
-import {
-  // -----------------------数据定义
-  ref,
-  reactive,
-  toRefs,
-
-  // -----------------------provide、inject
-  provide,
-  inject,
-
-  // -----------------------slots、attrs
-  useSlots,
-  useAttrs,
-
-  // -----------------------watch、watchEffect、computed
-  watch,
-  watchEffect,
-  computed,
-
-  // -----------------------其他（尽量不要用）
-  getCurrentInstance,
-
-  // -----------------------生命周期（常用）
-  // 挂载前
-  onBeforeMount,
-  // 挂载后
-  onMounted,
-  // 卸载前
-  onBeforeUnmount
-} from 'vue'
+// import {
+//   // -----------------------数据定义
+//   ref,
+//   reactive,
+//   toRefs,
+//
+//   // -----------------------provide、inject
+//   provide,
+//   inject,
+//
+//   // -----------------------slots、attrs
+//   useSlots,
+//   useAttrs,
+//
+//   // -----------------------watch、watchEffect、computed
+//   watch,
+//   watchEffect,
+//   computed,
+//
+//   // -----------------------其他（尽量不要用）
+//   getCurrentInstance,
+//
+//   // -----------------------生命周期（常用）
+//   // 挂载前
+//   onBeforeMount,
+//   // 挂载后
+//   onMounted,
+//   // 卸载前
+//   onBeforeUnmount
+// } from 'vue'
 </script>
 
 <style lang="scss" src="./index.scss" scoped></style>
