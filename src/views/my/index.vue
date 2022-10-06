4acedbc66e9ccf3c52446b9ca6f16e1a374af08d
李俊琦 <77267563@qq.com>
2022-10-06 17:18:54 +0800

    .

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="my">
-    <MyTableList />
+    <tmp />
+    <!-- <MyTableList /> -->
     <!-- <MyDialogForm /> -->
   </div>
 </template>
 
 <script setup lang="ts" name="my">
+import tmp from './tmp/index.vue'
 import MyTableList from './MyTableList/index.vue'
 import MyDialogForm from './MyDialogForm/index.vue'
 </script>
