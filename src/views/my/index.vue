4732f8eaf0012af957a351063fd0f37df189263d
李俊琦 <77267563@qq.com>
2022-10-12 17:44:18 +0800

    .

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -9,7 +9,6 @@
         <h3>具名slot-foo</h3>
       </template>
     </JsxComp>
-    <!-- <JsxComp2 @myEmit="myEmit"></JsxComp2> -->
   </div>
 </template>
 
@@ -17,7 +16,7 @@
 import tmp from './tmp/index.vue'
 import MyTableList from './MyTableList/index.vue'
 import MyDialogForm from './MyDialogForm/index.vue'
-import JsxComp from './JsxComp/JsxComp2.vue'
+import JsxComp from './JsxComp/JsxComp.vue'
 
 function myEmit(payload: any) {
   console.log('来自JsxComp的事件', payload)
